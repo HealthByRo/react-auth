3fbc5f33f7b6bed93a623598b4c0b5ca4dc8137b
Andrzej Dybionka <a.dybionka@redgran.com>
2019-09-03 14:33:15 +0200

    Jest config

---
+++ internals/testing/test-bundler.js
@@ -0,0 +1,45 @@
+// needed for regenerator-runtime
+// (ES7 generator support is required by redux-saga)
+import enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { JSDOM } from 'jsdom';
+
+const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
+const { window } = jsdom;
+
+function copyProps(src, target) {
+  const props = Object.getOwnPropertyNames(src)
+    .filter((prop) => typeof target[prop] === 'undefined')
+    .map((prop) => Object.getOwnPropertyDescriptor(src, prop));
+  Object.defineProperties(target, props);
+}
+
+global.window = window;
+global.document = window.document;
+global.navigator = {
+  userAgent: 'node.js',
+};
+copyProps(window, global);
+
+enzyme.configure({ adapter: new Adapter() });
+
+const localStorageMock = (() => {
+  let store = {};
+
+  return {
+    getItem(key) {
+      return store[key];
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    removeItem(key) {
+      delete store[key];
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+global.localStorage = localStorageMock;
