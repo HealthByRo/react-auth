7f5237aa1a2ac2123d7315f73bb308ee7a3ae68c
Andrzej Dybionka <a.dybionka@redgran.com>
2019-09-19 13:16:31 +0200

    useSignOutSync should send logout request to api only when isAuthenticated became false from true

--- src/containers/AuthenticationProvider/useSignOutSync/index.js
+++ src/containers/AuthenticationProvider/useSignOutSync/index.js
@@ -1,12 +1,16 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { signOut } from '../../../api';
 
 export default function useSignOutSync(isAuthenticated) {
+  const ref = useRef(false);
+
   return useEffect(() => {
-    if (isAuthenticated) {
+    if (!isAuthenticated && ref.current) {
       signOut().catch(() => {
         // TODO what to do on fail?
       });
     }
+
+    ref.current = true;
   }, [isAuthenticated]);
 }
